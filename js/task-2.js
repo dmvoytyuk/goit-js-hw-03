fb39479df98c08f497807859e461d26dbb775ee6
dmvoytyuk <dmvoytyuk@gmail.com>
2023-11-20 18:57:52 +0100

    task-2.js

---
+++ js/task-2.js
@@ -0,0 +1,17 @@
+function makeArray(firstArray, secondArray, maxLength) {
+	const mainArray = firstArray.concat(secondArray);
+	const lengthDifference  = mainArray.length - maxLength;
+	console.log(lengthDifference);
+	if(lengthDifference > 0) {
+		mainArray.length = mainArray.length - lengthDifference;
+		return mainArray;
+	}
+	return mainArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []		
